f501ac48c48b3d675d144158319ab64fcb677ca9
Idan <idan.zonshein@powerhrg.com>
2020-06-04 15:06:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Idan Zonshein"
+  end
+
+  get '/hometown' do
+    "My hometown is Warrington"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Monster-Mash"
+  end
 end
