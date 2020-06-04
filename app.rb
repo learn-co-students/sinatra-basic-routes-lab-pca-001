145f22a36f16967a2044c67012e6d8feea5c356c
Madison McCardle <madison.mccardle@powerhrg.com>
2020-06-04 16:14:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Maddie"
+  end
+
+  get '/hometown' do
+    "My hometown is Warrington"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is anything by Ariana Grande"
+  end
 end
